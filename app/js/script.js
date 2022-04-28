8a87814e4accb6955891ec0ce02421c8c12ab778
webdeveloper1213 <sameera_sulaiman@yahoo.com>
2022-04-28 13:20:14 -0700

    s1_branch

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log('Allah is the greatest')
+const btHamburger = document.querySelector("#btHamburger");
+btHamburger.addEventListener("click", () => {
+  console.log("clicked");
+  if (btHamburger.classList.contains("open")) {
+    btHamburger.classList.remove("open");
+  } else {
+    btHamburger.classList.add("open");
+  }
+});
